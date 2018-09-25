7dc55fcfceff716fb9ddd91f20789f80c3ca49e4
James Hosken <jameswfhosken@gmail.com>
2018-09-24 20:49:26 -0400

    Write user data to server file

--- server.js
+++ server.js
@@ -1,28 +1,75 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
+let express = require('express')
+let app, server,
+	path = require('path'),
+    host = process.env.HOST || '127.0.0.1',
+    port = process.env.PORT || 3000,
+    public = path.resolve(__dirname + "/public");
+app = express();
+app.use(express.static(public));
 
-app.get('/', (req, res) => res.sendfile('index.html'))
+//User agent parsing
+var useragent = require('useragent');
 
-// app.get('/', (req, res) => res.redirect('https://raw.githubusercontent.com/jameshosken/useless-data/master/LICENSE'))
+//File system
+var fs = require('fs');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Routes
+app.get('/', function(req,res){
 
+	console.log('New visit!');
 
-exports.logger = function(req, res) {
-  var user = {
-    agent: req.header('user-agent'), // User Agent we get from headers
-    referrer: req.header('referrer'), //  Likewise for referrer
-    ip: req.header('x-forwarded-for') || req.connection.remoteAddress, // Get IP - allow for proxy
-    screen: { // Get screen info that we passed in url post data
-      width: req.param('width'),
-      height: req.param('height')
-    }
-  };
-  // Store the user in your database
-  // User.create(user)...
-  console.log("User Data Recorded!");
-  console.log(user);
-  res.end();
+	let user = {
+		agent: useragent.parse(req.header('user-agent')), // User Agent we get from headers
+		referrer: req.header('referrer'), //  Likewise for referrer
+		ip: req.header('x-forwarded-for') || req.connection.remoteAddress, // Get IP - allow for proxy
+	};
+
+	console.log(user);
+
+	try{
+		let fileContents = "";
+		fs.readFile('data/raw.txt', function(err, buf )  {
+				//fileContents += buf.toString();
+				fileContents += buf.toString();
+				if(fileContents.length > 0){
+					console.log('>>> file found')
+			  		console.log("READING: " + fileContents);
+			  		writeContents(fileContents, user)
+			  		//Todo parse from JSON
+				}	
+		});
+	}catch(e){
+		//No file?
+		console.log('>>> no file found')
+		writeContents("", user);
+	}
+	res.sendfile('index.html');
+})
+
+app.get('*', function(req,res){
+	res.send("No data at this URL");
+})
+
+//Start server
+server = app.listen(port, host, serverStarted);
+
+function writeContents(originalContents, user){
+	console.log("Writing File");
+
+	//Append new data to content
+	//Overwrite previous file if exists
+	let outputData = originalContents + JSON.stringify(user) + ",";
+
+	//Todo parse to JSON
+	fs.writeFile('data/raw.txt', outputData, function(err, data){
+	    if (err) console.log(err);
+	    console.log("Successfully Written to File:");
+	    console.log(outputData);
+	});
+}
+
+function serverStarted () {
+    console.log('Server started', host, port);
+    console.log('Root directory', public);
+    console.log('Press Ctrl+C to exit...\n');
 }
